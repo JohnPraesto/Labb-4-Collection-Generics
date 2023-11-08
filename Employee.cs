3b65ea24f7db98e157cf80b3c2d40118ad7612ad
John Praesto <john_praesto@hotmail.com>
2023-11-08 11:42:18 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_4_Collection_Generics
+{
+    internal class Employee
+    {
+        private int _id;
+        private string _name;
+        private bool _isMale;
+        private int _salary;
+        public Employee(int id, string name, bool isMale, int salary)
+        {
+            _id = id;
+            _name = name;
+            _isMale = isMale;
+            _salary = salary;
+        }
+        public override string ToString()
+        {
+            string gender = IsMale(_isMale);
+            return $"{_id, -5}{_name,-10}{gender,-10}{_salary,-10}"; // Siffrorna avgör avståndet mellan utskrifterna. Minus betyder left alignt, inte minus betuder right aligned.
+        }
+        public static string IsMale(bool _isMale)
+        {
+            if (_isMale)
+                return "Male";
+            else
+                return "Female";
+        }
+        public static void IsMaleOne(List<Employee> empList)
+        {
+            Employee findMale = empList.Find(e => e._isMale == true);
+            Console.WriteLine(findMale.ToString()) ;
+        }
+        public static void IsMaleAll(List<Employee> empList)
+        {
+            List<Employee> findMales = new List<Employee>();
+            findMales = empList.FindAll(e => e._isMale == true);
+            foreach (Employee item in findMales)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Reflection;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using static System.Console; // Hämtar jag Console här behöver jag inte skriva det där nere hela tiden!!
+
+            //===========================//
+            //    JOHN PRAESTO SUT-23    //
+            //===========================//
+
+namespace Labb_4_Collection_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(001, "Gordon", true, 60000);
+            Employee emp2 = new Employee(002, "Esmeralda", false, 25000);
+            Employee emp3 = new Employee(003, "Rener", true, 30000);
+            Employee emp4 = new Employee(004, "Spencer", true, 220000);
+            Employee emp5 = new Employee(005, "Sarah", false, 42000);
+
+            while (true)
+            {
+                Clear();
+                WriteLine("To see Stack enter 1\nTo see List enter enything else");
+                string selection = ReadLine();
+                Clear();
+                if (selection == "1")
+                {
+                    Stack<Employee> employees = new Stack<Employee>();
+
+                    employees.Push(emp1);
+                    employees.Push(emp2);
+                    employees.Push(emp3);
+                    employees.Push(emp4);
+                    employees.Push(emp5); // Kan man pusha in dessa i nån sorts loop?
+
+                    WriteLine("Listing empolyees in stack without removing them. (ToString method)\n");
+                    WriteLine($"{"ID",-5}{"NAME",-10}{"GENDER",-10}{"SALARY",-10}{"STACK SIZE",-10}\n");
+                    foreach (Employee item in employees)
+                    {
+                        Write(item.ToString());
+                        WriteLine(employees.Count());
+                    }
+
+                    WriteLine("\nListing empolyees in stack and removing them. (Push method)\n");
+                    while (employees.Count > 0)
+                    {
+                        Write(employees.Pop());
+                        WriteLine(employees.Count());
+                    }
+
+                    employees.Push(emp1);
+                    employees.Push(emp2);
+                    employees.Push(emp3);
+                    employees.Push(emp4);
+                    employees.Push(emp5);
+
+                    WriteLine("\nLooking at top of stack without removing. (Peek method)\n");
+
+                    Write(employees.Peek());
+                    WriteLine(employees.Count());
+                    Write(employees.Peek());
+                    WriteLine(employees.Count());
+
+                    WriteLine("\nChecking if emp3 is in stack. (Contains method)\n");
+
+                    if (employees.Contains(emp3))
+                        WriteLine("emp3 is in stack");
+                    else
+                        WriteLine("emp3 is not in stack");
+
+                    WriteLine("\nPress Enter to return to menu");
+                    ReadLine();
+                }
+                else
+                {
+                    List<Employee> empList = new List<Employee>() { emp1, emp2, emp3, emp4, emp5 };
+
+                    WriteLine("\nChecking if emp2 is in list (Contains method):");
+
+                    if (empList.Contains(emp2))
+                        WriteLine("Employee 2 exists in the list");
+                    else
+                        WriteLine("Employee 2 does not exist in the list");
+
+                    WriteLine("\nPrinting first male in the list (Find method):");
+
+                    Employee.IsMaleOne(empList);
+
+                    WriteLine("\nPrinting all males in the list (FindAll method):");
+
+                    Employee.IsMaleAll(empList);
+
+                    WriteLine("\nPress Enter to return to menu");
+                    ReadLine();
+                }
+            }
+        }
+    }
+}
